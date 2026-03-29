60f5e1285418fb54332693a94f4b721a2bf0ced9
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2026-03-30 04:56:31 +0530

    fix: input code parse

--- src/stt/speech-translation-settings.ts
+++ src/stt/speech-translation-settings.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { input_audio_codec } from "./utils";
 
 /**
  * Specifies the speech generation model to use.
@@ -12,32 +13,7 @@ export type SpeechTranslationModelId =
 	| (string & {});
 
 export const speechTranslationSettingsSchema = z.object({
-	input_audio_codec: z.enum([
-		"wav",
-		"x-wav",
-		"wave",
-		"mp3",
-		"mpeg",
-		"mpeg3",
-		"x-mp3",
-		"x-mpeg-3",
-		"aac",
-		"x-aac",
-		"aiff",
-		"x-aiff",
-		"ogg",
-		"opus",
-		"flac",
-		"x-flac",
-		"mp4",
-		"x-m4a",
-		"amr",
-		"x-ms-wma",
-		"webm",
-		"pcm_s16le",
-		"pcm_l16",
-		"pcm_raw",
-	]),
+	input_audio_codec: input_audio_codec.nullish(),
 });
 
 export type SpeechTranslationSettings = {
@@ -47,9 +23,7 @@ export type SpeechTranslationSettings = {
 	 * Our API automatically detects all codec formats, but for PCM files specifically (pcm_s16le, pcm_l16, pcm_raw), you must pass this parameter.
 	 * PCM files are supported only at 16kHz sample rate.
 	 */
-	input_audio_codec?: z.infer<
-		typeof speechTranslationSettingsSchema.shape.input_audio_codec
-	>;
+	input_audio_codec?: z.infer<typeof input_audio_codec>;
 };
 
 export const speechTranslationResponseSchema = z.object({
