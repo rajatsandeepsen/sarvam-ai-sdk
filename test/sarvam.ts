dc6de0cc91521d58617799babe4f22ed9fca3cc6
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2026-04-29 19:44:36 +0530

    chore: version up

--- test/sarvam.ts
+++ test/sarvam.ts
@@ -1,9 +1,10 @@
 import { createSarvam } from "../src";
 
 export const sarvam = createSarvam({
+	// @ts-expect-error
 	fetch: (input: RequestInfo, init?: RequestInit) => {
 		console.log("Fetch called with:", input, init);
-		// throw new Error("Fetch is not implemented in this test environment.");
+		// throw new Error("Stop");
 		return fetch(input, init);
 	},
 });
